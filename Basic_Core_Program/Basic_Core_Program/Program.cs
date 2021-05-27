a1ad806a8f8605da1d034d6b87f89aff3ef4ae8a
Shreya Malviya <malviyashreya26@gmail.com>
2021-05-27 20:16:35 +0530

    [Shreya]Add: Welcome to Core Program

---
+++ Basic_Core_Program/Basic_Core_Program/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Basic Core Programs");
+            Console.ReadLine();
+        }
+    }
+}
