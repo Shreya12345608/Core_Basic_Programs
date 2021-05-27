e3a90fd28f74b7edc63894b7a0e86d30d0c9049b
Shreya Malviya <malviyashreya26@gmail.com>
2021-05-27 20:42:41 +0530

    [Shreya]Add: Check Year is Leap or not

--- Basic_Core_Program/Basic_Core_Program/Program.cs
+++ Basic_Core_Program/Basic_Core_Program/Program.cs
@@ -13,9 +13,9 @@ namespace Basic_Core_Program
 
             Console.WriteLine("Welcome to Basic Core Programs");
             Console.WriteLine("Enter your choice between 1 to 10: ");
-            Console.WriteLine("Press 1.Flip a Coin Head and Tail. ");
+            Console.WriteLine("1 For: Flip a Coin Head and Tail. ");
+            Console.WriteLine("2 For: Select Value is Leap Year Or not");
 
-            
             int selection = Convert.ToInt32(Console.ReadLine());
             // user selection
             switch (selection)
@@ -23,6 +23,10 @@ namespace Basic_Core_Program
                 case 1:
                     FlipCoinHeadOrTail FlipCoin = new FlipCoinHeadOrTail();
                     FlipCoin.CheckHeadTailPercentage();
+                    break;
+                case 2:
+                    CheckLeapYearOrNot LeapYearOrNot = new CheckLeapYearOrNot();
+                    LeapYearOrNot.LeapYear();
                     break;
                 default:
                     Console.WriteLine("Invalid Selection Select Between 1.");
