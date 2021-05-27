c7ebcd436e416dfa2ae70c8024d5639b8dcf3843
Shreya Malviya <malviyashreya26@gmail.com>
2021-05-27 20:32:20 +0530

    [Shreya]Add: Check Flip Coin is Head or Tail and Calculate %

---
+++ Basic_Core_Program/Basic_Core_Program/FlipCoinHeadOrTail.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core_Program
+{
+    class FlipCoinHeadOrTail
+    {
+        public int FlipHeads = 0;
+        public int FlipHails = 0;
+
+
+        public void CheckHeadTailPercentage()
+        {
+            Console.WriteLine("Enter How many times you want to Flip a Coin : ");
+            int FlipNumber = Convert.ToInt32(Console.ReadLine());
+
+            for (int coinFlip = 0; coinFlip <= FlipNumber; coinFlip++)
+            {
+                Random random = new Random();
+                double CheckFlipCoin = random.NextDouble();
+                if (CheckFlipCoin < 0.5)
+                {
+                    Console.WriteLine("Result: Is Head");
+                    FlipHeads++;
+                }
+                else
+                {
+                    Console.WriteLine("Result: Is Tail");
+                    FlipHails++;
+                }
+            }
+            double CheckPercentageTails = (FlipHeads * 100) / FlipNumber;
+            double CheckPercentageHeads = (FlipHails * 100) / FlipNumber;
+            Console.WriteLine("percentage of heads and tails is : " + CheckPercentageTails + " " + CheckPercentageHeads);
+            Console.ReadKey();
+        }
+        
+    }
+}
--- Basic_Core_Program/Basic_Core_Program/Program.cs
+++ Basic_Core_Program/Basic_Core_Program/Program.cs
@@ -12,6 +12,22 @@ namespace Basic_Core_Program
         {
 
             Console.WriteLine("Welcome to Basic Core Programs");
+            Console.WriteLine("Enter your choice between 1 to 10: ");
+            Console.WriteLine("Press 1.Flip a Coin Head and Tail. ");
+
+            
+            int selection = Convert.ToInt32(Console.ReadLine());
+            // user selection
+            switch (selection)
+            {
+                case 1:
+                    FlipCoinHeadOrTail FlipCoin = new FlipCoinHeadOrTail();
+                    FlipCoin.CheckHeadTailPercentage();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Selection Select Between 1.");
+                    break;
+            }
             Console.ReadLine();
         }
     }
